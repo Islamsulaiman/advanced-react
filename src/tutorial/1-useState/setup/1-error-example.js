f7ded68d3a55835188cc44426556be9f5c72bef2
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-15 13:28:20 +0300

    initial commit

--- src/tutorial/1-useState/setup/1-error-example.js
+++ src/tutorial/1-useState/setup/1-error-example.js
@@ -1,7 +1,19 @@
 import React from 'react';
 
 const ErrorExample = () => {
-  return <h2>useState error example</h2>;
+  let title = "random title";
+  let handleClick = () => {
+    console.log("handle click");
+  }
+  return (
+    <React.Fragment>
+      <h4>{title}</h4>
+      <button type="button" className="btn" onClick={handleClick}>
+        change text
+      </button>
+    </React.Fragment>
+
+  );
 };
 
 export default ErrorExample;
