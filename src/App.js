b9b90ef685fb898112a0b8a57a2e1c26aa7543af
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-18 14:03:45 +0300

    short circuit with react

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import UseStateBasics from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import UseStateBasics from "./tutorial/3-conditional-rendering/setup/2-short-circuit";
 
 function App() {
   return (
--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
-const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+//this component turn between and , or based on the value of text and using short circuit
 
-  return <h2>short circuit</h2>;
+const ShortCircuit = () => {
+  let [text, setText] = useState(false);
+
+  return (
+    <>
+      {text && <h3>And operator</h3>}
+      {text || <h3>Or operator </h3>}
+      <button className='btn' onClick={() => setText(!text)}>
+        Change the state
+      </button>
+    </>
+  );
 };
 
 export default ShortCircuit;
