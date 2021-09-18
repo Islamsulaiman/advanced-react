8c6bfd11c526df01ac17c6fa988d8378dd4a00a3
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-18 15:41:26 +0300

    Hide component part1

--- src/tutorial/3-conditional-rendering/setup/3-show-hide.js
+++ src/tutorial/3-conditional-rendering/setup/3-show-hide.js
@@ -1,7 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 
 const ShowHide = () => {
-  return <h2>show/hide</h2>;
+  let [bool, setBool] = useState(false);
+
+  return (
+    <>
+      <h3>{bool}</h3>
+      <button
+        className='btn'
+        onClick={() => {
+          setBool(!bool);
+        }}
+      >
+        Hide/appear Component
+      </button>
+    </>
+  );
+};
+
+const comp = () => {
+  return (
+    <>
+      <h3>Hello</h3>
+    </>
+  );
 };
 
 export default ShowHide;
