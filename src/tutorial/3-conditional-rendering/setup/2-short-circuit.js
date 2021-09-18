25bba75f0d5cef233592bae1f45894153eb90718
Islam Sulaiman <ma.eslam@yahoo.com>
2021-09-18 14:25:23 +0300

    using ternary operator eith react component

--- src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -2,18 +2,29 @@ import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
-//this component turn between and , or based on the value of text and using short circuit
-
+// this component turn between and , or based on the value of text and using short circuit
+//
 const ShortCircuit = () => {
   let [text, setText] = useState(false);
 
   return (
     <>
-      {text && <h3>And operator</h3>}
-      {text || <h3>Or operator </h3>}
+      <h3>Using ternary operator</h3>
+      {/* this button flip the value of text */}
       <button className='btn' onClick={() => setText(!text)}>
         Change the state
       </button>
+      <br />
+      {text ? (
+        <p>
+          The value is <bold>true</bold>
+        </p>
+      ) : (
+        <div>
+          <h5>The value is false</h5>
+          <bold>Blease check text value</bold>
+        </div>
+      )}
     </>
   );
 };
