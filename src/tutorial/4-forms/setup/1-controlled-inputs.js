749fb7171a64ec349f894b61db54e176e17943da
islam Sulaiman <ma.eslam@yahoo.com>
2021-09-18 23:27:10 +0300

    forms controling events part 1

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -1,11 +1,12 @@
 import React, { useState } from "react";
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
+
+/*This component will link between the value Iam entring with the value in the useState of any input feild
+by setting value and onChange() in every input feild */
 
 const ControlledInputs = () => {
+  let [firstName, setFirstName] = useState("");
+  let [email, setEmail] = useState("");
+
   const subFunc = (e) => {
     /*preventDefault will prevent the object er are in to do its default action, in forms the default is to re-render after submitting
     and we dont want that right now */
@@ -18,11 +19,25 @@ const ControlledInputs = () => {
       <form action='GET' className='form' onSubmit={subFunc}>
         <div>
           <label htmlFor='firstName'>Name: </label>
-          <input type='text' id='' name='firstName' />
+          <input
+            type='text'
+            id=''
+            name='firstName'
+            placeholder='First name'
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
         </div>
         <div>
           <label htmlFor='email'>Email: </label>
-          <input type='text' id='' name='email' />
+          <input
+            type='text'
+            id=''
+            name='email'
+            placeholder='Email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
         </div>
         <button type='submit'>Submit</button>
       </form>
